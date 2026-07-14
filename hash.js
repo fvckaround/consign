f073bd6c32760d39786471c33dff0a9f22d17630
kingsleyonyekwelu <kimonyekwelu@gmail.com>
2026-07-14 15:12:45 +0100

    Remove hash generator script

--- hash.js
+++
@@ -1,7 +0,0 @@
-const bcrypt = require("bcryptjs");
-const password = process.argv[2];
-if (!password) {
-  console.log("Usage: node hash.js YourPasswordHere");
-  process.exit(1);
-}
-console.log(bcrypt.hashSync(password, 10));
